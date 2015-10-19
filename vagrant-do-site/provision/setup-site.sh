46c324b9b919f8c661bbd2534b0a60e83851bd52
Kevin Porras <kporras07@gmail.com>
2015-10-19 16:16:51 -0600

    Some files movements and first attempt to update ubuntu tu 14.04

--- vagrant-do-site/provision/setup-site.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo ''
-echo '======================================================================='
-echo 'Running site setup...'
-cd /home/vagrant/playbooks
-echo 'Creating site'
-ansible-playbook --connection=local deploy/drupal-sites/drupal-dev-site-deploy.yml -i vagrant/hosts
